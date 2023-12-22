5bb20b1cb13f4152899fb5bb5a1064e5cb05b7c7
tkooooo123 <tkooooo123@gmail.com>
2023-12-22 17:32:23 +0800

    新增商品搜尋功能與搜尋頁面

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import { useContext, useEffect, useRef, useState } from "react";
 import { AuthContext } from '../store/AuthContext';
 import propTypes from 'prop-types';
 import { Collapse } from "bootstrap";
+import $ from 'jquery';
 
 
 function Header({ cartData }) {
@@ -13,6 +14,9 @@ function Header({ cartData }) {
     const [isToggled, setIsToggled] = useState(false);
     const navCollapse = useRef(null);
     const location = useLocation();
+    const searchRef = useRef(null);
+    const [keyword, setKeyword] = useState('');
+    const [keywords, setKeywords] = useState([]);
 
     const openCollapse = () => {
         navCollapse.current.show();
@@ -24,15 +28,39 @@ function Header({ cartData }) {
             navCollapse.current.hide();
         }
     }
+    const switchSearchCollapse = () => {
+        const searchCollapse = $('.search-collapse').css('display')
+        if (searchCollapse === 'none') {
+            $('.search-btn').addClass('s-active');
+            searchRef.current.show();
+        } else {
+            $('.search-btn').removeClass('s-active');
+            searchRef.current.hide();
+
+        }
+
+    }
+    const closeSearchCollapse = () => {
+        searchRef.current.hide();
+        $('.search-btn').removeClass('s-active');
+    }
+
+    const handleChange = (e) => {
+        setKeyword(e.target.value.trim())
+    }
 
     useEffect(() => {
         navCollapse.current = new Collapse('#navbarNav', {
             toggle: false,
         });
-
+        searchRef.current = new Collapse('#searchCollapse', {
+            toggle: false,
+        });
     }, [])
     useEffect(() => {
+        setKeyword('');
         closeCollapse();
+        closeSearchCollapse();  
     }, [location])
 
     return (
@@ -47,20 +75,15 @@ function Header({ cartData }) {
                                 openCollapse();
                             }
                         }}
-
                     >
                         <div className="navicon"></div>
                     </div>
-
-
-
 
                     <NavLink to="/" className="navbar-brand d-flex align-items-center"
                     >
                         <img src="./images/logo.png" alt="logo" />
                         <span className="ms-2 fs-4 fw-bold">BAKERY</span>
                     </NavLink>
-
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-3 ms-lg-0 h-100">
 
@@ -100,32 +123,66 @@ function Header({ cartData }) {
                             )}
                         </ul>
                     </div>
-
-
-
-
                     <ul className="icon-list d-flex justify-content-center mb-0">
-                        <NavLink to="/account/profile" className="icon-list-item text-black d-flex align-items-center me-3" >
+                        <div className="position-relative">
+                            <div className="icon-list-item search-btn d-flex align-items-center"
+                                onClick={switchSearchCollapse}
+                            >
+                                <span className="material-icons fs-2"
+                                >
+                                    search
+                                </span>
+                            </div>
+                            <div className="collapse search-collapse p-3 " ref={searchRef} id="searchCollapse">
+                                <div className="search-bar form-group d-flex">
+                                    <input type="text" className="form-control rounded-0" placeholder="請輸入商品關鍵字"
+                                        value={keyword}
+                                        onChange={(e) => handleChange(e)}
+                                    />
+                                    <Link className={`search-link nav-link ${keyword ? '' : 'disabled'}`} to={`/search?keyword=${keyword}`}
+                                        onClick={() => {
+                                            if (!keywords.includes(keyword) && keyword.length) {
+                                                setKeywords([...keywords, keyword])
+                                            }
+                                        }}
+                                    >
+                                        <span className="material-icons fs-3 px-1 ">
+                                            search
+                                        </span>
+                                    </Link>
+                                </div>
+                                <div className="mt-2">
+                                    {keywords.map((item) => {
+                                        return (
+                                            <Link className="p-1 fw-bold bg-white m-2 d-inline-block rounded-3" key={item}
+                                                to={`/search?keyword=${item}`}
+                                            >{item}</Link>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                        <NavLink to="/account/profile" className="icon-list-item d-flex align-items-center" >
                             <span className="material-icons fs-2">
                                 account_circle
                             </span>
                         </NavLink>
 
-                        <NavLink to="/cart" className="icon-list-item text-black d-flex align-items-center me-3 position-relative">
+                        <NavLink to="/cart" className="icon-list-item d-flex align-items-center me-3 position-relative">
                             <span className="material-icons fs-2">
                                 local_mall
                             </span>
                             {!!cartData?.carts?.length && (
-                                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                <span className="position-absolute cartItem-quantity translate-middle badge rounded-pill bg-danger">
                                     {cartData?.carts?.length}
                                 </span>
                             )}
                         </NavLink>
                         {!isAuthenticated && (
-                            <NavLink className="text-black fw-bold  d-none d-lg-flex" to="/login">登入/註冊</NavLink>
+                            <NavLink className="icon-list-item fw-bold  d-none d-lg-flex" to="/login">登入/註冊</NavLink>
                         )}
                         {isAuthenticated && (
-                            <NavLink className="text-black fw-bold d-none d-lg-flex" to="/login"
+                            <NavLink className="icon-list-item fw-bold d-none d-lg-flex" to="/login"
                                 onClick={(logout)}
                             >登出</NavLink>
                         )}
---
+++ src/pages/front/Search.jsx
@@ -0,0 +1,130 @@
+import { useEffect, useState, useContext } from 'react';
+import { Link, useSearchParams, useOutletContext, useNavigate } from 'react-router-dom';
+import ProductsApi from '../../apis/products';
+import CartApi from '../../apis/cart';
+import { MessageContext, handleErrorMessage, postSuccessMessage, authErrorMessage } from "../../store/messageStore";
+import { AuthContext } from '../../store/AuthContext';
+import Pagination from '../../components/Pagination';
+import Loading from '../../components/Loading';
+function Search() {
+    const { getCart } = useOutletContext();
+    
+    const [products, setProducts] = useState([]);
+    const [pagination, setPagination] = useState({});
+    const [searchParams] = useSearchParams();
+    const [isLoading, setIsLoading] = useState(true);
+    const [, dispatch] = useContext(MessageContext);
+    const auth = useContext(AuthContext);
+    const { isAuthenticated } = auth.user;
+    const navigate = useNavigate();
+
+    const keyword = searchParams.get('keyword');
+    const getProducts = async (page) => {
+        try {
+            setIsLoading(true);
+            const res = await ProductsApi.getProducts({
+                page,
+                categoryId: "",
+                keyword
+            })
+            if (keyword) {
+                setProducts(res.data.products);
+            }
+            setPagination(res.data.pagination);
+            if ( page > res.data.pagination.totalPage ) {
+                navigate(`/search?keyword=${keyword}`);
+            } else {
+                navigate(`/search?keyword=${keyword}&page=${page}`);
+            }
+
+            setIsLoading(false);
+        } catch (error) {
+            setIsLoading(false);
+            handleErrorMessage(dispatch, error);
+        }
+    }
+
+    const addToCart = async (id) => {
+        try {
+            setIsLoading(true);
+            if (!isAuthenticated) {
+                authErrorMessage(dispatch);
+                return
+            }
+            const res = await CartApi.postCart({
+                productId: id,
+                quantity: 1,
+            })
+            postSuccessMessage(dispatch, res.data);
+            await getCart()
+            setIsLoading(false);
+        } catch (error) {
+            setIsLoading(false);
+            handleErrorMessage(dispatch, error);
+        }
+    }
+
+    useEffect(() => {
+        getProducts(1);
+    }, [keyword]);
+    
+
+   
+
+    return (
+        <div className="container pt-66 mh">
+            <Loading isLoading={isLoading} />
+            <div className="row mt-3">
+                <div className="d-flex justify-content-between align-items-center">
+                    <h1 className="fw-bold">搜尋結果</h1>
+                    <p className="text-end"><Link className="text-black" to="/">首頁</Link> / <Link className="text-black" to="/products">本店商品</Link> / 搜尋結果</p>
+                </div>
+                <div className="col-md-3 d-flex d-md-block">
+                    <h4 className="mt-3 mb-3 fw-bold align-self-center">搜尋關鍵字：</h4>
+                    <span className="bg-white text-primary py-1 px-3 rounded-3 fw-bold fs-5 mt-lg-0 my-3">{keyword}</span>
+                </div>
+                <div className="col-md-9">
+                    <div className="row mt-3">
+                        {!products.length && (
+                            <p className="fs-2 fw-bold text-center">沒有符合條件的商品。</p>
+                        )}
+                        {products.map((product) => {
+                            return (
+                                <div className="col-lg-4 col-md-6 mb-3" key={product.id}>
+                                    <Link to={`/product/${product.id}`}>
+                                        <div className="product-card">
+                                            <div className="img-wrapper">
+                                                <img className="product-card-img" src={product.image} alt={product.name} />
+                                            </div>
+                                            <div className="card-body p-3">
+                                                <h5 className="product-card-title fw-bold">{product.name}</h5>
+                                                <p className="fw-bold text-danger">NT${product.price}</p>
+                                                <button className="btn btn-primary fw-bold"
+                                                    onClick={(e) => {
+                                                        e.preventDefault();
+                                                        addToCart(product.id);
+                                                    }}
+                                                >加入購物車</button>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    {!!products.length && (
+                         <div className="d-flex justify-content-center">
+                         <Pagination pagination={pagination} changePage={getProducts}/>
+                     </div>
+
+                    )}
+                   
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Search;
