040f717273d341c0a90de22e54f67e7b1a8f0e72
tkooooo123 <tkooooo123@gmail.com>
2024-02-16 16:13:22 +0800

    修正 Footer  css 樣式

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,34 +3,32 @@ function Footer() {
 
     return (
         <>
-            <footer className="bg-primary">
+            <footer className="footer bg-primary">
                 <div className="container">
                     <div className="row pt-4 px-md-0 px-3">
                         <div className="col-md-4 col-6">
                             <h4 className="fw-bold">關於我們</h4>
                             <ul className="d-flex flex-column ">
-                                <Link to="/brandstory" className="text-black">品牌故事</Link>
-                                <Link to="/contactus" className="text-black">聯絡我們</Link>
+                                <Link to="/brandstory" className="text-black fw-bold">品牌故事</Link>
+                                <Link to="/contactus" className="text-black fw-bold">聯絡我們</Link>
                             </ul>
                         </div>
                         <div className="col-md-4 col-6">
                             <h4 className="fw-bold">顧客服務</h4>
                             <ul className="d-flex flex-column ">
-                                <Link to="/frequencely_asked_questions" className="text-black">常見問題</Link>
-                                <Link to="/privacy" className="text-black">隱私權政策</Link>
+                                <Link to="/frequencely_asked_questions" className="text-black fw-bold">常見問題</Link>
+                                <Link to="/privacy" className="text-black fw-bold">隱私權政策</Link>
                             </ul>
                         </div>
                         <div className="col-md-4">
                             <h4 className="fw-bold">BAKERY</h4>
                             <ul className="d-flex flex-column ">
                                 <ul className="d-flex align-items-center">
-                                    <a href="#">
-                                        
-                                    </a>
+                                 
                                 </ul>
-                                <span >營業時間： 10:00 - 20:00</span>
-                                <span>電話：04-2345-6789</span>
-                                <span >Email： bakery_ec@gmail.com</span>
+                                <span className="fw-bold">營業時間： 10:00 - 20:00</span>
+                                <span className="fw-bold">電話：04-2345-6789</span>
+                                <span className="fw-bold">Email： bakery_ec@gmail.com</span>
                                 
                             </ul>
                         </div>
